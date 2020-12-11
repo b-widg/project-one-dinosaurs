9fa451882faf01ea5d3fdc57ff94aeaa39f15007
b-widg <b-widg@users.noreply.github.com>
2020-12-11 12:07:18 -0800

    feat: add function to toggle form visibility

--- app.js
+++ app.js
@@ -7,10 +7,9 @@ const Dino = function (species, weight, height, diet, where, when, fact) {
   this.where = where;
   this.when = when;
   this.fact = fact;
-  this.imageUrl = `./images/${species}.png`.toLowerCase();
+  this.imagePath = `./images/${species}.png`.toLowerCase();
 };
 
-// Create Dino Objects
 (async () => {
   const res = await fetch('./dino.json');
   const dinoData = await res.json();
@@ -29,8 +28,8 @@ const makeDinos = (dinoData) => {
       element.where,
       element.when,
       element.fact
-      // dino also has a url to it's image created by the constructor
-      // imageUrl = `./images/${species}.png`.toLowerCase();
+      // dino also has a path to it's image created by the constructor
+      // imagePath = `./images/${species}.png`.toLowerCase();
     );
 
     dinoArray.push(dino);
@@ -52,11 +51,17 @@ const makeDinos = (dinoData) => {
       {} // initial value of accumulator is empty object
     );
     console.log('formData:', formData);
-    makeHuman(formData);
+    makeHuman(formData); // make human object from data submitted by user
+    clearFormData(formElements); // clear form after form submission
+    toggleFormVisibility(); // Hide form after submission
   });
   // TODO remove from DOM
   // TODO validate data
 })();
+
+const clearFormData = (formElements) => {
+  formElements.forEach((element) => (element.value = ''));
+};
 
 const makeHuman = function (formData) {
   const human = {
@@ -66,7 +71,7 @@ const makeHuman = function (formData) {
     weight: formData.weight,
     diet: formData.diet,
     height: parseInt(feet) * 12 + parseInt(inches),
-    imageUrl: `./images/human.png`,
+    imagePath: `./images/human.png`,
   };
   console.log('human:', human);
 };
@@ -85,5 +90,21 @@ const makeHuman = function (formData) {
 // Add tiles to DOM
 
 // Remove form from screen
+//chose visibility = hidden over display = none to keep footer from jumping up
+// when form is hidden.
+// TODO try to fix delay in hiding button when hiding form
+const toggleFormVisibility = () => {
+  const form = document.getElementById('dino-compare');
+  form.style.visibility == ''
+    ? (form.style.visibility = 'hidden')
+    : (form.style.visibility = '');
+};
+
+// Function only to test toggleFormVisibility() by clicking "How do you compare?" header.
+// (() => {
+//   document
+//     .querySelector('body > header:nth-child(1) > h3:nth-child(3)')
+//     .addEventListener('click', (e) => toggleFormVisibility());
+// })();
 
 // On button click, prepare and display infographic
